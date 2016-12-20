adbd271871ae78ec58ed2f28208de0a83285756c
Peter Nanayon <pccowboyphils@gmail.com>
2016-12-21 03:13:21 +0800

    Fixed Image link

--- home-about.php
+++ home-about.php
@@ -10,7 +10,7 @@
             <div class="row">
             	<div class="xxAbout">
                 <div class="xxAbout-wrapper">
-						<img class="xxAboutPic" src="<?php echo htmlspecialchars(home_url());?>/wp-content/uploads/2010/07/IMG_20160916_195550-150x150-1.jpg"  />
+						<img class="xxAboutPic" src="<?php echo htmlspecialchars(home_url());?>/wp-content/uploads/2016/11/IMG_20160916_195550-150x150.jpg"  />
 						<p class="xxParagraph-intro"style="">
 							Hi there! Thank you for your time visiting my website. My name is <span class="xxAbout-name">Peter Nanayon</span> aka <span class="xxAbout-alias">PcCowboy</span>, a freelance web designer and developer living in Rosario, Pasig City, Philippines. I am very passionate about web designs and computer programming. I love to write codes and designs that is easy to understand,  extend and maintain. Below are the detailed lists of my skills.
 						</p>
